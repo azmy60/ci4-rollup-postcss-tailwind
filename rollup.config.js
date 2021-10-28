78376a681652b34164cb61d3012ba456bc8ca221
Mohammad Azmi <mazmiagus2003@gmail.com>
2021-10-28 19:53:46 +0700

    lint rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,29 +2,29 @@ import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import eslint from '@rollup/plugin-eslint';
 import replace from '@rollup/plugin-replace';
-import { terser } from "rollup-plugin-terser";
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/scripts/main.js',
   output: {
     file: 'public/scripts/main.js',
     format: 'iife',
-    sourcemap: (process.env.NODE_ENV === 'production' ? false : 'inline')
+    sourcemap: (process.env.NODE_ENV === 'production' ? false : 'inline'),
   },
   plugins: [
     resolve(),
     eslint({
       fix: true,
-      exclude: ['node_modules/**', 'src/styles/']
+      exclude: ['node_modules/**', 'src/styles/'],
     }),
     babel({
       exclude: 'node_modules/**',
-      babelHelpers: 'bundled' 
+      babelHelpers: 'bundled',
     }),
     replace({
       preventAssignment: true,
       exclude: 'node_modules/**',
-      ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+      ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
     }),
-    (process.env.NODE_ENV === 'production' && terser())]
+    (process.env.NODE_ENV === 'production' && terser())],
 };

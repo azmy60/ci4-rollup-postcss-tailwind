392823da51439e2a992d0df0483643ba906c8665
Mohammad Azmi <mazmiagus2003@gmail.com>
2021-10-30 13:01:44 +0700

    Fix database auto increment

--- app/Modules/setup/sql/database.sql
+++ app/Modules/setup/sql/database.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `todos` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `completed` tinyint(1) NOT NULL DEFAULT '0'
+  `completed` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;

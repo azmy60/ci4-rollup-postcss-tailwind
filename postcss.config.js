51ed485329908865292194792aa4255420a97217
Mohammad Azmi <mazmiagus2003@gmail.com>
2021-10-30 21:00:46 +0700

    Refactor postcss.config.js
    and add cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies, global-require */
 // TODO use es6
-const plugins = [
-  require('stylelint'),
-  require('postcss-import'),
-  require('tailwindcss/nesting'),
-  require('tailwindcss'),
-  require('postcss-reporter')({ clearReportedMessages: true }),
-];
 
 module.exports = {
-  plugins: process.env.NODE_ENV === 'production' ? [...plugins, require('autoprefixer'), require('stylelint')] : plugins,
+  plugins: [
+    require('stylelint')(),
+    require('postcss-import')(),
+    require('tailwindcss/nesting')(),
+    require('tailwindcss')(),
+    require('postcss-reporter')({ clearReportedMessages: true }),
+    process.env.NODE_ENV === 'production' ? require('autoprefixer')() : null,
+    process.env.NODE_ENV === 'production' ? require('cssnano')({ preset: 'default' }) : null,
+  ],
 };

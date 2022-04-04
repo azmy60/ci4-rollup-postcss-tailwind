6c145393c19bf8f74c72673753016d5d31b935e1
Mohammad Azmi <mazmiagus2003@gmail.com>
2022-04-04 20:49:24 +0700

    refactor storage and TaskModel test

--- src/js/storage.js
+++ src/js/storage.js
@@ -1,5 +1,7 @@
 const clear = (key) => localStorage.removeItem(key);
+
 const load = (key, defaultValue) => JSON.parse(localStorage.getItem(key)) ?? defaultValue;
+
 const save = (key, value, replacer = null) => {
   const stringified = JSON.stringify(value, replacer);
   localStorage.setItem(key, stringified);
